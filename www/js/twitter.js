9d7c3455dafe575d843e05c757541e509bc11b8a
nednal12 <nednal12@fullsail.edu>
2012-11-17 22:54:47 -0500

    Update www/js/twitter.js
    Terminal update wasn't working. Updated via gitub edit window.

--- www/js/twitter.js
+++ www/js/twitter.js
@@ -1,14 +1,16 @@
 //Brent Marohnic
 // AVF 1211
-// Wait for Cordova to load
+// Full Sail University
 //
+//
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady (){
 }
 
 $(function () {
-  $.getJSON("http://search.twitter.com/search.json?q=tony%20horton&from=Tony_Horton&rpp=5&include_entities=true&result_type=recent&callback=?",
+  $.getJSON("http://search.twitter.com/search.json?q=tony%20horton&from=Tony_Horton&rpp=6&include_entities=true&result_type=recent&callback=?",
             function(data) {
 			console.log(data);
 			$("#pTweets").text("Got Your Tweets!!!");
@@ -16,8 +18,13 @@ $(function () {
             for (i=0, j=data.results.length; i<j; i++)
 			// alert(data.results[i].text);
 			
-			$("#ulTweets").append("<li>" +
+			$("#ulTweets").append("<li class= 'sub' id='twit" + i + "'>" +
+                                  "<p class='ts'>" +
+                                  data.results[i].created_at +
+                                  "</p>" +
+                                  "<p class='sub'>" +
                                   data.results[i].text +
+                                  "</p>" +
                                   "</li>"
                                   );
             });
